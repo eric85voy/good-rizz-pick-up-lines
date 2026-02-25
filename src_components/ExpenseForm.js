d1c96c5c7c0699ae3577d2dbbafe70578daa7844
eric85voy <eric85voy@outlook.com>
2026-02-25 11:42:08 +0530

    Create ExpenseForm.js

---
+++ src_components/ExpenseForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+function ExpenseForm() {
+  return (
+    <div className="form-container">
+      <p>Add your own rizz line feature coming soon...</p>
+    </div>
+  );
+}
+
+export default ExpenseForm;
